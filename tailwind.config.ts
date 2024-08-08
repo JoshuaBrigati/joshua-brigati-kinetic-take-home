98465c6654a7881a2dd0ec6fe9666a5ef9ece3cc
Joshua Brigati <brigati.joshua@gmail.com>
2024-08-07 20:47:48 -0500

    inital setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "hsla(var(--primary))",
+        destructive: "hsla(var(--destructive))",
+        "grey-0": "hsla(var(--grey-0))",
+        "grey-50": "hsla(var(--grey-50))",
+        "grey-100": "hsla(var(--grey-100))",
+        "grey-300": "hsla(var(--grey-300))",
+        "grey-400": "hsla(var(--grey-400))",
+        "grey-500": "hsla(var(--grey-500))",
+        "grey-600": "hsla(var(--grey-600))",
+        "grey-650": "hsla(var(--grey-650))",
+        "grey-700": "hsla(var(--grey-700))",
+        "grey-800": "hsla(var(--grey-800))",
+        "grey-900": "hsla(var(--grey-900))",
+        "grey-1000": "hsla(var(--grey-1000))",
+        white: "rgba(var(--white))",
+      }
     },
   },
   plugins: [],
